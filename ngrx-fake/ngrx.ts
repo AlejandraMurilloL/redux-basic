33ca86e0911794cfe97b36c4e6a9a3595dedc7df
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-09-30 16:52:56 -0500

    Se implementa el store junto con getSatet y dispatch

--- ngrx-fake/ngrx.ts
+++ ngrx-fake/ngrx.ts
@@ -2,3 +2,7 @@ export interface Action {
     type: string;
     payload?: any; // parametros que podemos enviarle a la acción
 }
+
+export interface Reducer<T> {
+    (state: T, action: Action): T;
+}
